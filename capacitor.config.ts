e9300235c1fc73de327cef875634f5ea4b48db14
MigueleugiM26 <129754678+MigueleugiM26@users.noreply.github.com>
2023-11-22 21:40:02 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Estácio Bank',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
